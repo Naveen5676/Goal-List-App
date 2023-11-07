abc9253dc8d2b686343d732e0bbda1a48c90cd0f
Naveen5676 <sample.first@yahoo.com>
2023-11-07 13:26:26 +0530

    Dynamic Styling using classname & template letral

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -3,8 +3,11 @@ import React from 'react';
 import './Button.css';
 
 const Button = props => {
+  const btnchange = props.btnchange;
+  
+
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={`button ${!btnchange ? 'error':''}`} onClick={props.onClick}>
       {props.children}
     </button>
   );
